03dd959e030d12c27e3d3b00757910a3d17d975c
IAmEvyatar <evyatarsilberschatz@gmail.com>
2018-12-07 03:39:18 +0200

    adds near done version of 04_12_18 - 004.js

--- home_work/04_12_18/js/004.js
+++ home_work/04_12_18/js/004.js
@@ -1,16 +1,16 @@
-end = 0;
-arraya = ['Hi', 6, 'string', 2, (45 + 2), 55, 'Yo'];
-arrName = prompt('Name of array to copy from');
-ind = Number(prompt('Strarting number'));
-end = Number(prompt('Ending number (optional)'));
+	myArr = ['Hi', 6, 'string', 2, (45 + 2), 55, 'Yo'];
+	myArr2 = ['Yo', 0, 'Wazzup', 13, 14, 15, 16, 17];
+	arrName = prompt('Name of array to copy from');
+	ind = Number(prompt('Strarting number'));
+	end = Number(prompt('Ending number (optional)'));
 function mySlice(arrName, ind, end) {
 	newArr = [];
 	if(end == 0 || end == NaN) {
 		end = arrName.length;
 	}
-	
-	for(ind; ind <= end && ind <= arrName.length; ind++) {
-		arrName[ind] == undefined ? newArr[ind] == 'undefined' : console.log(arrName[ind]);
+	for(ind -= 1; ind < end && ind <= arrName.length; ind++) {
+		arrName[ind] == undefined ? newArr[ind] == 'undefined' : 
+		newArr[ind] = eval(arrName)[ind];
 	}
 	return newArr;
 }
